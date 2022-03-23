0e181b83dd970647c838f2409a568cf60d4f432a
fiansky <samuel.asare@azubiafrica.org>
2022-03-23 05:31:43 +0000

    stdio.h added to main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+#include <stdio.h>
+
 void rev_string(char *s);
 
 void reset_to_98(int *n);
@@ -22,5 +24,7 @@ void puts2(char *str);
 void puts_half(char *str);
 
 void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
 
 #endif
